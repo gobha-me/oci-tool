65821a548e3ffa7dc68df264b6ec394a7384eddc
gitlab-runner <svc-gitlabRunner@evoforge.org>
2021-02-22 20:43:44 +0000

    Formatted version of Commit SHA 72cda8557234b51b4613eb760864dfd861b4d469. Pushed by Gitlab CI. autoformatted

--- include/OCI/Manifest.hpp
+++ include/OCI/Manifest.hpp
@@ -5,7 +5,7 @@
 namespace OCI {
   template < class Schema_t > // Schema_t is an object that has attributes name and mediaType and an overloaded
                               // from_json
-                              auto Manifest( OCI::Base::Client *client, Schema_t const &request ) -> Schema_t;
+  auto Manifest( OCI::Base::Client *client, Schema_t const &request ) -> Schema_t;
 } // namespace OCI
 
 template < class Schema_t > auto OCI::Manifest( OCI::Base::Client *client, Schema_t const &request ) -> Schema_t {
