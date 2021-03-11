700fd9fd7c2247a1c45657eccdd993d1bea98c13
edmonat <emonat@redhat.com>
2021-03-11 15:41:35 -0500

    Changed to trying to use python file to determine correct version and release for rpmspec and subbing that into a template file for builds.

---
+++ rpmbuild/rpmSpecBuild.py
@@ -0,0 +1,22 @@
+/usr/bin/env python
+import os
+print("Beginning python script to determine version and release")
+# Get Env vars of build from OS
+CI_TAG = os.getenv('CI_COMMIT_TAG')
+CI_BRANCH = os.getenv('CI_COMMIT_BRANCH')
+CI_SHSHA = os.getenv('CI_COMMIT_SHORT_SHA')
+# Little if-else to ensure we have valid version and release
+if CI_BRANCH == "master":
+    try:
+        version = CI_TAG
+        release = "rel"
+    except:
+        version = CI_SHSHA
+        release = "beta"
+else:
+    version = CI_SHSHA
+    release = "devel"
+print(version, release)
+# Output version and release to os env
+os.environ['RPM_VERSION'] = 'version'
+os.environ['RPM_RELEASE'] = 'release'
