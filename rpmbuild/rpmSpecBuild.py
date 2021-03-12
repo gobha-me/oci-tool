f449afa0a4e435a4bfdad7250e8bb4fb238fa523
edmonat <emonat@redhat.com>
2021-03-12 09:55:11 -0500

    Changes to the python script for setting up the rpmbuild. Changed all the file+permissions operations to use a function. notest

--- rpmbuild/rpmSpecBuild.py
+++ rpmbuild/rpmSpecBuild.py
@@ -1,10 +1,14 @@
-#!/bin/python
+#!/usr/python
 import os
+import shutil
 print("Beginning python script to determine version and release")
 # Get Env vars of build from OS
 CI_TAG = os.getenv('CI_COMMIT_TAG')
 CI_BRANCH = os.getenv('CI_COMMIT_BRANCH')
 CI_SHSHA = os.getenv('CI_COMMIT_SHORT_SHA')
+def funcFCopyAndPermissions( inputF, outputF, a="*", followS="follow_symlinks=True"):
+    shutil.copyfile(inputF, outputF)
+    shutil.copymode(inputF, outputF)
 # Little if-else to ensure we have valid version and release
 if CI_BRANCH == "master":
     try:
@@ -18,8 +22,10 @@ else:
     release = "devel"
 print(version, release)
 # Output version and release to os env
-os.environ['RPM_VERSION'] = 'version'
-os.environ['RPM_RELEASE'] = 'release'
+#os.environ['RPM_VERSION'] = 'version'
+#os.environ['RPM_RELEASE'] = 'release'
+RPM_VERSION = 'version'
+RPM_RELEASE = 'release'
 # Read file into mem
 with open('/home/gitlabRunner/rpmbuild/SPECS/oci-sync-TEMPLATE.spec', 'r') as file :
     filedata = file.read()
@@ -31,25 +37,13 @@ with open('/home/gitlabRunner/rpmbuild/SPECS/oci-sync-TEMPLATE.spec', 'w') as fi
 # Create properly-named directory for build
 if not os.path.exists("/home/gitlabRunner/oci-sync-"+RPM_VERSION):
     os.makedirs("/home/gitlabRunner/oci-sync-"+RPM_VERSION)
-# Collect files for the rpmbuild TODO: Make this a function
-shutil.copyfile(/home/gitlabRunner/gitPull/oci-tool/LICENSE, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/", *, follow_symlinks=True)
-shutil.copymode(/home/gitlabRunner/gitPull/oci-tool/LICENSE, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/", *, follow_symlinks=True)
-shutil.copyfile(/home/gitlabRunner/build_files/build/bin/oci-sync, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/oci-sync.bin", *, follow_symlinks=True)
-shutil.copymode(/home/gitlabRunner/build_files/build/bin/oci-sync, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/oci-sync.bin", *, follow_symlinks=True)
-shutil.copyfile(/home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync.man, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/", *, follow_symlinks=True)
-shutil.copymode(/home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync.man, "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/", *, follow_symlinks=True)
+# Collect files for the rpmbuild
+funcFCopyAndPermissions( "/home/gitlabRunner/gitPull/oci-tool/LICENSE", "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/LICENSE")
+funcFCopyAndPermissions( "/home/gitlabRunner/build_files/build/bin/oci-sync","/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/oci-sync.bin")
+funcFCopyAndPermissions( "/home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync.man", "/home/gitlabRunner/oci-sync-"+RPM_VERSION+"/oci-sync.man")
+# Make call out to os to execute rpmdev-setuptree
 os.system('rpmdev-setuptree')
-# Collect more files for the rpmbuild TODO: No, really, make this a function
-- cp /home/gitlabRunner/gitPull/oci-tool/LICENSE ~/rpmbuild/SOURCES/
-- cp /home/gitlabRunner/build_files/build/bin/oci-sync ~/rpmbuild/SOURCES/oci-sync.bin
-- cp /home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync.man ~/rpmbuild/SOURCES/
-- tar -czf ~/oci-sync ./oci-sync-$RPM_VERSION
-    # Ensure all files are in the right place for the rpmbuild
-- mv ~/oci-sync ~/rpmbuild/SOURCES/
-- cd /home/gitlabRunner/rpmbuild/SOURCES
-- gunzip -c oci-sync | tar xvf -
-- cp /home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync-TEMPLATE.spec ~/rpmbuild/SPECS/
-# rpmbuild
-- cd ~/rpmbuild/SPECS/
-- rpmbuild -ba oci-sync-TEMPLATE.spec
-- rm -rf /home/gitlabRunner/oci-sync-$RPM_VERSION
+# Collect more files for the rmbuild
+funcFCopyAndPermissions( "/home/gitlabRunner/gitPull/oci-tool/LICENSE", "/home/gitlabRunner/gitPull/oci-tool/rpmbuild/SOURCES/LICENSE")
+funcFCopyAndPermissions( "/home/gitlabRunner/build_files/build/bin/oci-sync", "/home/gitlabRunner/gitPull/oci-tool/rpmbuild/SOURCES/oci-sync.bin")
+funcFCopyAndPermissions( "/home/gitlabRunner/gitPull/oci-tool/rpmbuild/oci-sync.man", "/home/gitlabRunner/gitPull/oci-tool/rpmbuild/SOURCES/oci-sync.man")
